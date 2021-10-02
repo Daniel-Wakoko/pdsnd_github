081bfdf45455db909da9122c181680ec97a99717
Daniel Wakoko <wernestdaniel@gmail.com>
2021-10-02 08:13:38 +0300

    Improve the heading comment for question 1

--- Project2.R
+++ Project2.R
@@ -30,18 +30,17 @@ wash$city <- "Washington"
 #merge all datasets into one
 cities <- rbind(chi, ny, wash)
 
-#4 User Info
-##What are the counts for each user type?
-
+#1 What are the counts for each user type?
+#=============================================================================================
 by(cities$User.Type, cities$city, summary)
 
 
 library(ggplot2)
-ggplot(data = cities, aes(User.Type)) + 
+ggplot(data = cities, aes(User.Type)) +
   geom_bar() +
-  facet_wrap(~city, ncol = 3) + 
-  ggtitle("Counts of User Types for Each City") + 
-  xlab("User Types") + 
+  facet_wrap(~city, ncol = 3) +
+  ggtitle("Counts of User Types for Each City") +
+  xlab("User Types") +
   ylab("Count of User Type")
 
 
@@ -50,25 +49,25 @@ ggplot(data = cities, aes(User.Type)) +
 summary(ny$Start.Station)
 
 #
-ssn <- data.frame(table(ny$Start.Station)) 
+ssn <- data.frame(table(ny$Start.Station))
 ny.freq <- ssn[which.max(ssn$Freq), ]
 ny.freq[1, 1]
 ny.freq$Var1
 
 #
-ss.chi <- data.frame(table(chi$Start.Station)) 
+ss.chi <- data.frame(table(chi$Start.Station))
 chi.freq <- ss.chi[which.max(ss.chi$Freq), ]
 chi.freq[1, 1]
 chi.freq$Var1
 
 #
-ss.wash <- data.frame(table(wash$Start.Station)) 
+ss.wash <- data.frame(table(wash$Start.Station))
 wash.freq <- ss.wash[which.max(ss.wash$Freq), ]
 wash.freq[1, 1]
 wash.freq$Var1
 #
 get.freq.SS <- function(p = wash){
-  ss.p <- data.frame(table(p$Start.Station)) 
+  ss.p <- data.frame(table(p$Start.Station))
   p.freq <- ss.p[which.max(ss.p$Freq), ]
   p.freq[1, 1]
   return(p.freq[1, 1])
@@ -86,13 +85,13 @@ ss.all2 <- subset(ss.all, Var1 == "Streeter Dr & Grand Ave" | Var1 == "Pershing 
 
 #
 cities2 <- subset(cities, Start.Station == "Streeter Dr & Grand Ave" | Start.Station == "Pershing Square North" | Start.Station == "Columbus Circle / Union Station")
-ggplot(data = cities2, aes(cities2$Start.Station))  + 
-  geom_bar() + 
-  ggtitle("Frequencies for the most common Stations in each city") + 
-  xlab("Start Station") + 
-  ylab("Frequency") 
+ggplot(data = cities2, aes(cities2$Start.Station))  +
+  geom_bar() +
+  ggtitle("Frequencies for the most common Stations in each city") +
+  xlab("Start Station") +
+  ylab("Frequency")
 
-  
+
 #What is the most common month?
 #============================================================================================
 cities
@@ -112,15 +111,15 @@ cities$Months <- cities$Start.Time %>%
                   months.Date()
 
 #
-ggplot(cities, aes(Months)) + 
-  geom_bar() + 
+ggplot(cities, aes(Months)) +
+  geom_bar() +
   ggtitle("Monthly Number of Rides by City") +
   ylab("Number of Rides") +
-  facet_wrap(~city, ncol = 2) 
+  facet_wrap(~city, ncol = 2)
 
 MostCommonMonth <- function(city = Chicago ,tt = " "){
   cities <- subset(cities, cities$city == city)
-  df = data.frame(table(cities$Month)) 
+  df = data.frame(table(cities$Month))
   df[which.max(df$Freq), ]
   print(tt)
   return(df[which.max(df$Freq), ][1, 1])
